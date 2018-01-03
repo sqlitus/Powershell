d36dbfff2f2e91a5a515bc8a7d76c00eda1a3208
sqlitus <jcjabr@gmail.com>
2018-01-03 09:20:49 -0600

    Add files via upload

--- Powershell Library.ps1
+++ Powershell Library.ps1
@@ -2,3 +2,9 @@ Powershell
 
 # list all items matching string, select all their properties
 ls -fil all*.csv | select *
+
+# text search within files
+ls -fil all*.csv | select-string t.*?t
+
+# search through folders, open
+ls ../../work -r -fil *powershell-lib* | invoke-item
